2b7252e3052b2112794342187436bb83b308fb5b
AlexBowmanCoding <alex@itsabox.net>
2022-08-26 17:29:56 -0400

    added navigation part 1

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import Main from "./screens/MainComponent";
+import { NavigationContainer } from '@react-navigation/native';
+import { Platform, View } from 'react-native';
+import Constants from 'expo-constants';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
 
 export default function App() {
-  return <Main/>;
+  return (
+  <NavigationContainer>
+    <Main />
+  </NavigationContainer>
+  )
 }
 
